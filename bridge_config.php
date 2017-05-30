6f4e84193c3d3eb0a214f7e33101334b9c793961
Paulo <paulo.m.tavares@gmail.com>
2017-05-30 10:15:53 +0100

    Refactor: removed neon references on datasources and files

--- bridge_config.php
+++ bridge_config.php
@@ -8,7 +8,7 @@ global $CFG;
 $KUINK_BRIDGE_CFG->loginhttps = false;
 $KUINK_BRIDGE_CFG->wwwroot = 'http://localhost/kuink/';
 $KUINK_BRIDGE_CFG->dirroot = '/var/www/html/kuink';
-$KUINK_BRIDGE_CFG->dataroot = '/opt/moodledata';
+$KUINK_BRIDGE_CFG->dataroot = '/opt/kuinkdata/';
 $KUINK_BRIDGE_CFG->kuinkroot = '';
 $KUINK_BRIDGE_CFG->theme = 'adminLTE2';
 
@@ -18,14 +18,14 @@ $currentRole = 'Teacher';
 $roles [] = $currentRole;
 // $roles[] = 'framework.admin';
 
-$KUINK_BRIDGE_CFG->application = 'kuink.pos'; // Default application
+$KUINK_BRIDGE_CFG->application = 'framework.entity'; // Default application
 $KUINK_BRIDGE_CFG->configuration = '<Configuration/>';
 
 $KUINK_BRIDGE_CFG->auth->roles = $roles;
 $KUINK_BRIDGE_CFG->auth->isAdmin = false;
 $KUINK_BRIDGE_CFG->auth->currentRole = $currentRole;
 
-$KUINK_BRIDGE_CFG->auth->user->id = 2460;
+$KUINK_BRIDGE_CFG->auth->user->id = 1;
 $KUINK_BRIDGE_CFG->auth->user->firstName = 'Demo';
 $KUINK_BRIDGE_CFG->auth->user->lastName = 'User';
 $KUINK_BRIDGE_CFG->auth->user->lang = 'en';

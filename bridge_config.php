a66b2d87ca55b9da24dd0647de9a8de50a215a98
Paulo <paulo.m.tavares@gmail.com>
2018-05-19 23:27:28 +0100

    small fix

--- bridge_config.php
+++ bridge_config.php
@@ -5,6 +5,7 @@ $KUINK_INCLUDE_PATH = realpath ( '' ) . '/kuink-core/';
 
 global $KUINK_BRIDGE_CFG;
 global $CFG;
+$KUINK_BRIDGE_CFG = new stdClass;
 $KUINK_BRIDGE_CFG->loginhttps = false;
 $KUINK_BRIDGE_CFG->wwwroot = 'http://localhost/kuink/';
 $KUINK_BRIDGE_CFG->dirroot = '/var/www/html/kuink';
@@ -21,15 +22,20 @@ $roles [] = $currentRole;
 $KUINK_BRIDGE_CFG->application = 'framework.page'; // Default application
 $KUINK_BRIDGE_CFG->configuration = '<Configuration/>';
 
+$KUINK_BRIDGE_CFG->auth = new stdClass;
+$KUINK_BRIDGE_CFG->auth->user = new stdClass;
 $KUINK_BRIDGE_CFG->auth->roles = $roles;
 $KUINK_BRIDGE_CFG->auth->isAdmin = false;
 $KUINK_BRIDGE_CFG->auth->currentRole = $currentRole;
 
-$KUINK_BRIDGE_CFG->auth->user->id = 1;
-$KUINK_BRIDGE_CFG->auth->user->firstName = 'Demo';
+$KUINK_BRIDGE_CFG->auth->user->id = 0;
+$KUINK_BRIDGE_CFG->auth->user->firstName = 'Guest';
 $KUINK_BRIDGE_CFG->auth->user->lastName = 'User';
-$KUINK_BRIDGE_CFG->auth->user->lang = 'en';
+$KUINK_BRIDGE_CFG->auth->user->lang = 'pt';
 $KUINK_BRIDGE_CFG->auth->sessionKey = null;
 
+$KUINK_BRIDGE_CFG->trigger = new stdClass; //The url to set in breadcrumb after home. On other bridges this is the external point where kuink was triggered. Allow get back to that url
+$KUINK_BRIDGE_CFG->trigger->url = null;
+$KUINK_BRIDGE_CFG->trigger->label = null;
 // ################################ KUINK END #######################################
 ?>
--- view.php
+++ view.php
@@ -6,17 +6,16 @@ date_default_timezone_set ( 'UTC' );
 session_start ();
 // ################################ KUINK START #######################################
 global $KUINK_INCLUDE_PATH;
-$KUINK_INCLUDE_PATH = realpath ( '' ) . '/kuink-core/';
-
 global $KUINK_BRIDGE_CFG;
+$KUINK_INCLUDE_PATH = realpath ( '' ) . '/kuink-core/';
 
 include ('./bridge_config.php');
 
-if ($_SESSION ['kuink.logged'] == 0)
+if (isset($_SESSION ['kuink.logged']) && ($_SESSION ['kuink.logged'] == 0))
 	$KUINK_BRIDGE_CFG->application = 'framework.login';
 else {
 	//Inject the user from framework.login application through session object
-	$user = $_SESSION ['kuink.logged.user']; //Comes from login application
+	$user = isset($_SESSION ['kuink.logged.user']) ? $_SESSION ['kuink.logged.user'] : null; //Comes from login application
 	$KUINK_BRIDGE_CFG->auth->user->id = $user['id'];
 	$KUINK_BRIDGE_CFG->auth->user->firstName = $user['firstName'];
 	$KUINK_BRIDGE_CFG->auth->user->lastName = $user['lastName'];
